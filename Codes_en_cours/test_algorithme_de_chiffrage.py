d58039234f8018c55095267a5c7e248fafb8d9f7
e2400734 <chloebrisse8@gmail.com>
2025-11-22 11:27:59 +0100

    CRéation du code pour obtenir un nombre premier sécurisé et aléatoire

--- Codes_en_cours/test_algorithme_de_chiffrage.py
+++ Codes_en_cours/test_algorithme_de_chiffrage.py
@@ -1,2 +1,11 @@
 from sympy import randprime
-print(randprime(100, 1000))
+import secrets
+def generation_nb_premier():
+    """
+    la fonction permet de générer quatre nombres premier au hasard compris entre 1.000 et 100.000,
+    ils sont rangés dans une liste ""liste"("(ligne 1). Puis est choisie au hasard l'élement de la liste.
+    :return: nombre premier tiré au hasard
+    """
+    liste = [randprime(1000, 100000) for _ in range(4)]
+    return secrets.choice(liste)
+
