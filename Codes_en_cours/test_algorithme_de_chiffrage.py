cecfebbed359f94a0b1487332aa8541cf90fd795
e2400734 <chloebrisse8@gmail.com>
2025-11-22 11:50:52 +0100

    Modification du code pour obtenir un nombre premier sécurisé et aléatoire. (utilisation de set)

--- Codes_en_cours/test_algorithme_de_chiffrage.py
+++ Codes_en_cours/test_algorithme_de_chiffrage.py
@@ -3,11 +3,12 @@ import secrets
 def generation_nb_premier():
     """
     La fonction permet de générer quatre nombres premiers au hasard compris entre 1.000 et 100.000,
-    Ils sont rangés dans une liste ""liste"("(ligne 1). Puis est choisie au hasard l'élément de la liste.
+    ils sont rangés dans un set "nbr"(ligne 1). Puis est choisie au hasard l'élément de la liste.
     La fonction secrets.choice permet une meilleure protection que random.
+    Il faut néanmoins traduire le set en tuple pour pouvoir le lire.
     :retourn : nombre premier tiré au hasard
     """
-    liste = [randprime(1000, 100000) for _ in range(4)]
-    return secrets.choice(liste)
+    nbr = {randprime(1000, 100000) for _ in range(4)}
+    return secrets.choice(tuple(nbr))
 print(generation_nb_premier())
 
