1f6d6b5ff22e64c7d856bc28d8c4df2240d090da
e2400734 <chloebrisse8@gmail.com>
2025-11-26 12:00:14 +0100

    finalisation de l'algorithme de chiffrage

---
+++ Codes_en_cours/routeur.py
@@ -0,0 +1,13 @@
+"""
+Socket, Thread, QT et MariaDB
+Déchiffrement d'une seul couche
+"""
+"""
+envoiyer son @ip
+envoi le port d'écoute
+envoie la clé publique
+
+le routeur doit écouter sur un port fixe
+chaque message reçu = thread dédié
+(voir aide chat-gpt)
+"""
